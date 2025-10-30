529055ddad45ded1a94dc82ab328b68692789a09
RonakYadavbash <yajagoronisa679585@gmail.com>
2025-10-30 18:03:49 +0530

    Create 23BCS12249_9_1.js

---
+++ 23BCS12249_9_1.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 React Dockerized Application</h1>
+      <p>This React app is running inside a Docker container using multi-stage build.</p>
+    </div>
+  );
+}
+
+export default App;
