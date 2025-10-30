dca933da902e93a7c5e56b3e0e52a0c6b1416308
RonakYadavbash <yajagoronisa679585@gmail.com>
2025-10-30 17:59:24 +0530

    Create 23BCS12249_7_2.js

---
+++ 23BCS12249_7_2.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+      const existingItem = state.items.find(i => i.id === item.id);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        state.items.push({ ...item, quantity: 1 });
+      }
+      state.totalQuantity += 1;
+      state.totalPrice += item.price;
+    },
+    removeFromCart: (state, action) => {
+      const id = action.payload;
+      const existingItem = state.items.find(i => i.id === id);
+      if (existingItem) {
+        state.totalQuantity -= 1;
+        state.totalPrice -= existingItem.price;
+        if (existingItem.quantity === 1) {
+          state.items = state.items.filter(i => i.id !== id);
+        } else {
+          existingItem.quantity -= 1;
+        }
+      }
+    },
+    clearCart: (state) => {
+      state.items = [];
+      state.totalQuantity = 0;
+      state.totalPrice = 0;
+    },
+  },
+});
+
+export const { addToCart, removeFromCart, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
